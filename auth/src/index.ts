d4798825ef5e318a57a75b0f2df4416496abf9cb
Sandeep Chugh <sandeep.chugh@live.com>
2020-08-26 22:48:16 -0500

    updated index.ts to support mongoose connection to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import {json} from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
@@ -22,7 +23,21 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-svc:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+    } catch( err ) {
+        console.error(err);
+    }
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!');
-});
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!');
+    });
+};
+
+start();
+
