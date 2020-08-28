5edd0229e83dbbd1a2c26cd3a93e404ab782f491
Sandeep Chugh <sandeep.chugh@live.com>
2020-08-27 23:49:00 -0500

    added sign in and current user get api

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,7 +7,7 @@ import {json} from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
-import {signupRouter} from './routes/singnup';
+import {signupRouter} from './routes/signup';
 import {errorHandler} from './middlewares/error-handler';
 import {NotFoundError} from './errors/not-found-error';
 
