4a1e7abaf981aca7bc30710112afecdbbf994210
sandeep chugh <sandeep.chugh@live.com>
2020-09-01 22:39:43 -0500

    added tickets folder and initial config

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+
+});
+
+it('can only be accessed if the user is signed in', async () => {
+
+});
+
+it('returns an error if an invalid title is provided', async () => {
+
+});
+
+it('returns an error if an invalid price is provided', async () => {
+
+});
+
+it('creates a valid ticket with valid inputs', async () => {
+
+});
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+
+});
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+
+});
