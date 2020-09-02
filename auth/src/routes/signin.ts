0af2b077b733f2cb432250b54956c06005581c35
sandeep chugh <sandeep.chugh@live.com>
2020-09-01 21:09:34 -0500

    refactored solution, extracted common code to /common

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest } from '@sandeepchugh/common';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError } from '@sandeepchugh/common';
 import { Password } from '../services/password';
 
 const router = express.Router();
